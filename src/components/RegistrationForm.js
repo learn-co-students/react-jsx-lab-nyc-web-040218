836d21a9ea2392221ae3202be960300350466f72
celestegerard <celeste.gerard@gmail.com>
2018-06-03 11:52:33 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const RegistrationForm = (props) => {
+  return (
+    <form>
+      <input type="text" />
+      <input type="password" />
+      <button type="submit"></button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
