dd791e17923f899c36ad4050c93bc864cf13f9ef
myungbeans <michael.h.myung@gmail.com>
2018-06-04 09:25:54 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+
+class FillerText extends React.Component{
+    render(){
+        return(
+            <p>I am filler text. I can be used to fill your screen. Amazing!</p>
+        )
+    }
+}
+
+export default FillerText
