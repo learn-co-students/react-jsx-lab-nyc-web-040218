03bb2778c0905d012ec2e8f0154a5c79d7ef00e1
Ahamed <ahamedali95@gmail.com>
2018-06-04 16:31:09 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,13 @@
+import React from "react";
+    // In the components/FillerText.js file, create a React component called FillerText.
+    // This component should be a paragraph containing the following text: I am a filler text. I can be used to fill your screen. Amazing!
+
+class FillerText extends React.Component {
+  render() {
+    return (
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+    );
+  }
+}
+
+export default FillerText;
